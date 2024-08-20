bbaff35186996ff6c4d6f943de8840806b5f9acd
vikas <vikas785singh@gmail.com>
2024-08-20 15:26:50 +0530

    rotating logo and quickliks

--- src/Home.js
+++ src/Home.js
@@ -45,21 +45,40 @@ const Home = () => {
   ]
 
   const footerRightLink = [
-    'Our Philosophy','About Us','Residential Developments','Commercial Developments','Contact Us'
+    {
+      href: 'our_philosophy',
+      text: 'Our Philosophy'
+    },
+    {
+      href: 'about_us',
+      text: 'About Us'
+    },
+    {
+      href: 'residential',
+      text: 'Residential Developments'
+    },
+    {
+      href: 'commercial',
+      text: 'Commercial Developments'
+    },
+    {
+      href: 'contact_us',
+      text: 'Contact Us'
+    }
   ]
 
   const footerLeftLink = [
     {
-      icon:'bi-geo-alt',
-      label:'Lotus Tower, Road No. 12 A'
+      icon: 'bi-geo-alt',
+      label: 'Lotus Tower, Road No. 12 A'
     },
     {
-      icon:'bi-envelope',
-      label:'sales@lotusdevelopers.com'
+      icon: 'bi-envelope',
+      label: 'sales@lotusdevelopers.com'
     },
     {
-      icon:'bi-telephone',
-      label:'+91-9987509311'
+      icon: 'bi-telephone',
+      label: '+91-9987509311'
     }
   ]
 
@@ -68,7 +87,7 @@ const Home = () => {
 
       <div className="px-5 fw-light">
         <ImageContainer imgHeight='400px' imgSrc="/assets/Banner1.jpg" />
-        <div className="row mt-2">
+        <div className="row mt-2" id='our_philosophy'>
           <div className="col lotus-text-grey" >
             <div className='fw-light' style={{ fontSize: '45px' }}>
               Mumbai's Premier <br /> Real Estate Developers
@@ -79,10 +98,10 @@ const Home = () => {
               {/* </span> */}
 
               <div className='d-flex my-3'>
-              <div className='text-center rounded-circle ' style={{width:'40px', height:'40px', backgroundColor: 'white'}}>
-              <i class={`bi bi-telephone-fill  fs-3 `} style={{color:'black'}}></i>
-              </div>
-              <div className='ms-2'>Contact us</div>
+                <div className='text-center rounded-circle ' style={{ width: '40px', height: '40px', backgroundColor: 'white' }}>
+                  <i class={`bi bi-telephone-fill  fs-3 `} style={{ color: 'black' }}></i>
+                </div>
+                <div className='ms-2'>Contact us</div>
               </div>
             </div>
             <span className='lotus-text-golden fw-normal lotus-box px-4  py-1' style={{ fontSize: '28px' }}>
@@ -103,7 +122,7 @@ const Home = () => {
 
 
         <div id="carouselExampleSlidesOnly " className="carousel slide mt-5" data-bs-ride="carousel">
-          <div className="carousel-inner">
+          <div className="carousel-inner" id='about_us'>
             {
               aboutArray.map((aboutData, index) => {
                 return (
@@ -137,26 +156,28 @@ const Home = () => {
 
         </div>
 
-        <ImageContainer imgHeight='400px' imgSrc="/assets/Banner2.jpg" />
-        <div className="row mt-3 ">
-          <div className="col-5 text-white" >
-            <div className='fw-normal lh-1' style={{ fontSize: '50px' }}>Residential Developments</div>
+        <div id='residential'>
+          <ImageContainer imgHeight='400px' imgSrc="/assets/Banner2.jpg" />
+          <div className="row mt-3 " >
+            <div className="col-5 text-white" >
+              <div className='fw-normal lh-1' style={{ fontSize: '50px' }}>Residential Developments</div>
 
 
 
-          </div>
-          <div className="col-7 text-white ">
-            <div style={{ fontSize: '36px' }}>The Atlantis</div>
-            <div className='lh-1.2' style={{ fontSize: '24px' }}>
-              Atlantis is a one-of-a-kind project. It's uniqueness comes from the sheer scale and grandness of it. Limited 36 residences will be home to Mumbai's elite families. Flourished with an endless list of amenities, Atlantis caters to your every need.
             </div>
+            <div className="col-7 text-white ">
+              <div style={{ fontSize: '36px' }}>The Atlantis</div>
+              <div className='lh-1.2' style={{ fontSize: '24px' }}>
+                Atlantis is a one-of-a-kind project. It's uniqueness comes from the sheer scale and grandness of it. Limited 36 residences will be home to Mumbai's elite families. Flourished with an endless list of amenities, Atlantis caters to your every need.
+              </div>
 
 
+            </div>
           </div>
         </div>
 
 
-        <div className="row mt-5 pt-5">
+        <div className="row mt-5 pt-5" id='commercial'>
           <div className="col-5 text-white" >
             <div className='mt-5 pt-5 ms-5'>
               <div className='fw-normal mt-5 lh-1' style={{ fontSize: '50px' }}>Commercial Developments</div>
@@ -181,52 +202,54 @@ const Home = () => {
       </div>
 
       <div className=' row mt-5'>
-          {FooterArray.map(footerImg=>{
-            return(
-              <div className='col-3 p-0'>
+        {FooterArray.map(footerImg => {
+          return (
+            <div className='col-3 p-0'>
               <img src={footerImg} width='100%' alt="" />
             </div>
           )
-          })}
-          
+        })}
+
 
       </div>
 
-      <div className='container px-5 text-white'>
-          <div className='mx-5 mt-5 py-5 row'>
-            <div className='col-4 '>
-              {footerLeftLink.map(data=>{
-                return(
-                  <div className='my-4 d-flex'>
-              <div className='text-center rounded-circle ' style={{width:'48px', height:'48px', backgroundColor: '#D8953C'}}>
-              <i class={`bi ${data.icon} fs-2`} style={{color:'black'}}></i>
-              </div>
-              <div className='ps-2  d-flex align-items-center fs-6 lh-1'>{data.label} </div>
-              </div>
-                )
-              })}
-              
-              
-              
-
-            
-            </div>
-            <div className='col-4 text-center'>
-              <img src='/assets/Logo.png' width='250px' />
-            </div>
-            <div className='col-4 pt-4' >
-              {footerRightLink.map(data=>{
-                return(
-                  <div className='my-2'>
-                  <img src='/assets/icons/ArrowBullet.png'/>
-            <span className='ms-2' style={{fontSize: '20px'}}>{data}</span>
+      <div className='container px-5 text-white' id='contact_us'>
+        <div className='mx-5 mt-5 py-5 row'>
+          <div className='col-4 '>
+            {footerLeftLink.map(data => {
+              return (
+                <div className='my-4 d-flex'>
+                  <div className='text-center rounded-circle ' style={{ width: '48px', height: '48px', backgroundColor: '#D8953C' }}>
+                    <i class={`bi ${data.icon} fs-2`} style={{ color: 'black' }}></i>
                   </div>
-            
-                )
-              })}
-            
-            </div>
+                  <div className='ps-2  d-flex align-items-center fs-6 lh-1'>{data.label} </div>
+                </div>
+              )
+            })}
+
+
+
+
+
           </div>
+          <div className='col-4 text-center'>
+            <img src='/assets/Logo.png' width='250px' />
+          </div>
+          <div className='col-4 pt-4' >
+            {footerRightLink.map(data => {
+              return (
+                <div className='my-2'>
+                  <a href={`#${data.href}`} className='text-decoration-none'>
+                    <img src='/assets/icons/ArrowBullet.png' />
+                    <span className='ms-2 text-white ' style={{ fontSize: '20px' }}>{data.text}</span>
+                  </a>
+                </div>
+
+              )
+            })}
+
+          </div>
+        </div>
       </div>
 
 
--- src/component/ImageContainer.js
+++ src/component/ImageContainer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ImageContainer = ({ imgSrc, imgHeight}) => {
     return (
-        <div>
-            <img className='clip-polygon' src={imgSrc} alt="Paris" width="100%" height={imgHeight} />
+        <div style={{position:'relative'}}>
+            <img src='/assets/icons/logo.png' height='50px' className='rotate-image' width='50px' style={{position:'absolute', right: '200px', top:'0px'}} />
+            <img className='clip-polygon' src={imgSrc} alt="Paris" width="100%" height={imgHeight}  />
         </div>
     )
 }
